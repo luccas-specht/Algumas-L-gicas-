3f9ab98f4e669b84a8883da8aaefe62753a0b3bb
luccas-specht <luccasspecht70@gmail.com>
2024-03-05 14:00:42 -0300

    feat: add changes

---
+++ interview-questions/merge-sort-without-zeros.cpp
@@ -0,0 +1,13 @@
+// Given a unsorted arr provide a function calls merge sort without zeros which returns the arr sorted with the zeros at left  
+
+// [33, -8, 0, 4, 7, 0, 20, 1, 0, 4] -> [0, 0, 0, -8, 1, 4, 4, 7, 20, 33]
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void mergeSortWithoutZeros(vector<int> & nums, left, right) {
+
+}
--- searching/binary-search.cpp
+++ searching/binary-search.cpp
@@ -15,34 +15,31 @@
 
 using namespace std;
 
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
 
-bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(vector<int> arr: matrix) {
+       while (low <= high) {
+        int mid = low + high / 2;
 
-            int low = 0, high = arr.size() -1;
-
-            while(low <= high) {
-                int mid = low + high / 2;    
-
-                if (target == arr[mid]){
-                    return true;
-                } else if (target > arr[mid]){
-                    low = mid + 1;
-                } else if (target < arr[mid]){
-                    high = mid -1;
-                }   
-            }
+        if (target == nums[mid]){
+            return mid;
         }
 
-        return false;
-    }
+        if (target > nums[mid]){
+            low = mid + 1;
+        }
+      
+        if (target < nums[mid]){
+            high = mid - 1;
+        }
+	    }
+		  return -1;
+   }
 
 int main() {
-    vector<vector<int>> arr = {{1,3,5,7},
-                              {10,11,16,20},
-                              {23,30,34,60}};
+    vector<int> arr = {10,11,16,20};
    
-     bool booleano = searchMatrix(arr, 16);
+     int booleano = search(arr, 16);
 
     cout << boolalpha << booleano << endl;
     
